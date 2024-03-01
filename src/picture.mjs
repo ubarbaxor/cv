b0fa85231f54fa8e8b7f5a83a5f4fb471a3e1028
ubarbaxor <ubarbaxor@gmail.com>
2024-03-02 01:25:52 +0200

    Done

--- src/picture.mjs
+++ src/picture.mjs
@@ -1,16 +1,48 @@
-let src
-const img = document.createElement('img')
-img.id = 'info-picture'
-img.width = 200
-img.height = 200
+const srcImg = document.createElement('img')
+const outImg = document.createElement('img')
+outImg.id = 'info-picture'
 
-const themeQuery = window.matchMedia("(prefers-color-scheme)")
-console.log({ themeQuery })
+const canvas = document.createElement('canvas')
+canvas.width = 200
+canvas.height = 200
+const ctx = canvas.getContext('2d')
 
-const renderPicture = url => {
-    img.src = url
+const processImage = img => {
+    ctx.drawImage(img,
+        0,0,img.width,img.height,
+        0,0,canvas.width,canvas.height,
+    )
 
-    return img
+    const imageData = ctx.getImageData(
+        0,0,
+        canvas.width, canvas.height
+    )
+
+    for (let pxStart = 0; pxStart < imageData.data.length; pxStart += 4) {
+        const red = imageData.data[pxStart]
+        const grn = imageData.data[pxStart + 1]
+        const blu = imageData.data[pxStart + 2]
+        const avg = (red + grn + blu) / 3
+
+        imageData.data[pxStart + 3] = Math.max(0xf0 - avg, 0)
+    }
+    ctx.putImageData(imageData, 0, 0)
+
+    return canvas.toDataURL()
+}
+
+const renderPicture = src => {
+    const url = new URL(src)
+    srcImg.crossOrigin = url.origin
+    srcImg.src = url
+    srcImg.addEventListener('load', _ => {
+        outImg.src = processImage(srcImg)
+    })
+    srcImg.addEventListener('error', _ => {
+        outImg.src = './static/pp.nft.alpha.png'
+    })
+
+    return outImg
 }
 
 export default renderPicture
